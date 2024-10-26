02f3b2d84719545fb1f44bbdef37cbdd6d92fc8b
abdullah-25 <imv.abdullah@mail.utoronto.ca>
2024-10-26 19:48:34 -0400

    update package.json

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/main/", // Add this line - replace with your repository name
-  assetsInclude: ["**/*.mov", "**/*.MP4", "**/*.png", "**/*.PNG"], // Include your assets
+  base: "/supreme-car-detailing/", // Your repository name
+  assetsInclude: ["**/*.mov", "**/*.MP4", "**/*.png", "**/*.PNG"],
 });
