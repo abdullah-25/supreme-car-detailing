b0a6d027340646e55bb6efca31bcb0b394c703be
abdullah-25 <imv.abdullah@mail.utoronto.ca>
2024-10-26 19:39:48 -0400

    add github pages config

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,9 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const videos = [
-  "/videos/video1.mov",
-  "/videos/video2.mov",
-  "/videos/video3.mov",
-];
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: [
-    "**/*.PNG",
-    "**/*.png",
-    "**/*.jpg",
-    "**/*.jpeg",
-    "**/*.gif",
-    "**/*.svg",
-  ],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          videos: videos.map((v) => v.video),
-        },
-      },
-    },
-  },
+  base: "/main/", // Add this line - replace with your repository name
+  assetsInclude: ["**/*.mov", "**/*.MP4", "**/*.png", "**/*.PNG"], // Include your assets
 });
